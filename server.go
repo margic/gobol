0fe1e1d32011b71ebee7ba9324ce997074bc2e8c
Paul Crofts <pmcrofts@gmail.com>
2016-06-07 12:27:55 -0700

    Adding hello get function

--- server.go
+++ server.go
@@ -2,10 +2,30 @@ package main
 
 import (
   log "github.com/Sirupsen/logrus"
+  "net/http"
+  "github.com/matryer/respond"
+  "github.com/gorilla/mux"
 )
 
 func main() {
   log.WithFields(log.Fields{
     "Test": "Hello it worked",
   }).Info("Hello")
+
+  s := http.Server{
+    Addr: ":8080",
+  }
+
+  r := mux.NewRouter()
+  r.HandleFunc("/", HomeHandler)
+  http.Handle("/", r)
+
+  s.ListenAndServe()
+}
+
+func HomeHandler(w http.ResponseWriter, r *http.Request) {
+  log.WithFields(log.Fields{
+    "Request": r,
+  }).Debug("New Request")
+  respond.With(w, r, http.StatusOK, "Test")
 }
